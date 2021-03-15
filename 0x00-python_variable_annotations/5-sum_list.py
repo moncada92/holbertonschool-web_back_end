26301956ef6de85bf37733b4e1daaf676ff375d9
Camilo Moncada <monk-da@hotmail.com>
2021-03-15 17:43:25 -0500

    Complex types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 3
+""" function sum value type float in list """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ sum value the array """
+    return sum(input_list)
