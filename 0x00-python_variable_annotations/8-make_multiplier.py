3b34354a2014abc553a88c291c136f3095516189
Camilo Moncada <monk-da@hotmail.com>
2021-03-15 18:22:13 -0500

    complex types - functions hotfix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Complex type - function """
 
-from typing import Callable
+from typing import Callable, Union, Tuple
 
 
-def make_multiplier(multipler: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ return mutliply with function"""
     def func(number: float) -> float:
-        return number * multipler
+        return number * multiplier
 
     return func
