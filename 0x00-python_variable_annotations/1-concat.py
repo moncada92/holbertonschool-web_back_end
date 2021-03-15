22f4c637d7c14af020e3d12cc0f930e27d893665
Camilo Moncada <monk-da@hotmail.com>
2021-03-15 12:44:53 -0500

    Basic annotations contact

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" basic annotations - contact """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ return join the two strings """
+    return "{} {}".format(str1, str2)
