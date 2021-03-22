3a8647b5a32c001c16242224b103af37c39ae9b2
Camilo Moncada <monk-da@hotmail.com>
2021-03-22 09:00:40 -0500

    hotfix 3 question 0

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,16 +9,19 @@ class BasicCache(BaseCaching):
     Caching system class
     """
 
-    def __init__(self):
-        BaseCaching.__init__(self)
-
     def put(self, key, item):
+        """
+        add to the cache
+        """
         if (key is None or item is None):
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        get the cache item value
+        """
         if(key is None or self.cache_data.get(key) is None):
             pass
         else:
