18d13b17465ec850ecc3976c7fcd1f5de3630f09
Camilo Moncada <monk-da@hotmail.com>
2021-03-29 18:28:24 -0500

    response question 2

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ import re
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 class RedactingFormatter(logging.Formatter):
     """
     Redacting Formatter class
@@ -42,3 +45,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    get_logger function
+    source: https://realpython.com/python-logging/
+    """
+    # name of the logger
+    logger = logging.getLogger("user_data")
+    # set level to INFO
+    logger.setLevel(logging.INFO)
+    # propagate message
+    logger.propagate = False
+    # create the handle
+    c_handler = logging.StreamHandler()
+    # change format
+    c_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    # add handler
+    logger.addHandler(c_handler)
+    return logger
