4dea1d59556ba2a8669e1cb2fcabc8e44eb48846
Camilo Moncada <monk-da@hotmail.com>
2021-03-30 19:16:54 -0500

    pycodestyle question 3

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Module Authentication """
+
 from flask import request
 from typing import List, TypeVar
 
@@ -8,10 +9,15 @@ class Auth:
     """class Auth"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ required auth  """
         return False
 
+
     def authorization_header(self, request=None) -> str:
+        """ authorization header  """
         return None
 
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """ current User  """
         return None
