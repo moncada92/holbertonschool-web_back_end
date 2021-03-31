8c80b08241aa2ac5d213b245ec8de9500fef36ec
Camilo Moncada <monk-da@hotmail.com>
2021-03-30 19:18:35 -0500

    pycodestyle fix 2 question 3

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,11 +12,9 @@ class Auth:
         """ required auth  """
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """ authorization header  """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current User  """
