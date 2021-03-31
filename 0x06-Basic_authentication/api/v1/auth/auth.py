5890f4de610e8a4988e92c02c8eba8fac468c25e
Camilo Moncada <monk-da@hotmail.com>
2021-03-30 20:51:50 -0500

    response question 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,6 +10,13 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ required auth  """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths or path+'/' in excluded_paths:
+            return False
+        else:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
