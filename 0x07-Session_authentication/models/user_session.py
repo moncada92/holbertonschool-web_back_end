e99091b3fdbe1618793eaa9f13d5ec151a6341ac
Camilo Moncada <monk-da@hotmail.com>
2021-04-07 22:08:46 -0500

    hotfix 2 pycodestyle

--- 0x07-Session_authentication/models/user_session.py
+++ 0x07-Session_authentication/models/user_session.py
@@ -7,5 +7,6 @@ class UserSession(Base):
     """ class user session """
     def __init__(self, *args: list, **kwargs: dict):
         """ constructor """
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
