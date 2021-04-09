1dd5d50a2d75f053569a5696798be9dac1a25cd0
Camilo Moncada <monk-da@hotmail.com>
2021-04-09 17:15:15 -0500

    response question 0 and 1

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Implement User class in DB
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from user import Base, User
+
+
+class DB:
+    """ class DB """
+
+    def __init__(self):
+        """ contructor """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ set session """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ add user """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
