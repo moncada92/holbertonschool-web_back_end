c7075d40ea0d0e82eb51c1c02f1f1b4ba0570cf2
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 13:57:06 -0500

    bugfix 2 question 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -45,10 +45,10 @@ def logout():
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
 
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    abort(403)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
