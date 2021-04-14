bb8365524e9e917a4f6ee6dfc28bdf6148a0bba3
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 13:53:02 -0500

    response question 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ module routes """
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -38,6 +38,18 @@ def login():
         return response
 
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.user_id)
+        return redirect('/')
+    abort(403)
 
 
 if __name__ == "__main__":
--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -63,8 +63,9 @@ class Auth:
             return user
         except NoResultFound:
             return None
+        return None
 
     def destroy_session(self, user_id: int) -> None:
         """ destroy session """
-        self._db.update_user(user_id, session_id=None)
-        return None
+        if user_id:
+            self._db.update_user(user_id, session_id=None)
