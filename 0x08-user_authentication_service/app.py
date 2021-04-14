3784906977dc26f5f9f8aeb280403c4066173726
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 13:55:40 -0500

    bugfix question 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -43,11 +43,10 @@ def login():
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     session_id = request.cookies.get('session_id')
-
     user = AUTH.get_user_from_session_id(session_id)
 
     if user:
-        AUTH.destroy_session(user.user_id)
+        AUTH.destroy_session(user.id)
         return redirect('/')
     abort(403)
 
