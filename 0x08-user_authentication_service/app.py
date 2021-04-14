a6055f9cc14cf6f225a06616d83837353e41b873
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 09:50:08 -0500

    bugfix question 11

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ module routes """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -23,6 +23,20 @@ def userPost():
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception as error:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+
+    abort(401)
 
 
 if __name__ == "__main__":
