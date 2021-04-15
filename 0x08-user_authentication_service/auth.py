088d0d113521885ff445b643d2e87cffce611d3f
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 19:34:34 -0500

    bugfix6 question 19

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,18 +72,22 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """ get reset password token """
-        if email:
-            try:
-                user = self._db.find_user_by(email=email)
-                token = _generate_uuid()
-                self._db.update_user(user.id, reset_token=token)
-                return user.reset_token
-            except NoResultFound:
-                raise ValueError
+        """
+            Generate token to reset password
+            Return token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=str(uuid.uuid4()))
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """ udpate password """
+        """
+            Reset password
+            Return None
+        """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             password = _hash_password(password)
