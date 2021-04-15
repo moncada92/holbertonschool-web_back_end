08260c4c4141514549ef3655710176be23bb4550
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 19:37:14 -0500

    bugfix7 question 19

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python3
-""" module routes """
-
+"""
+Flask app
+"""
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
-AUTH = Auth()
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route('/')
-def index() -> str:
+@app.route('/', methods=['GET'], strict_slashes=False)
+def simple_get() -> str:
+    """
+        GET /
+        Return: simple message
+    """
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def userPost():
+def users() -> str:
+    """
+        POST /users
+        Return message error or success
+    """
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception as error:
-        print(error)
         return jsonify({"message": "email already registered"}), 400
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login():
+def login() -> str:
+    """
+        Create a session user
+        Return response
+    """
     email = request.form.get('email')
     password = request.form.get('password')
-
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie('session_id', session_id)
         return response
-
     abort(401)
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
+    """
+        Destroy session
+        Abort if id not exists or redirect
+    """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
@@ -52,14 +65,17 @@ def logout():
 
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
-def profile():
-
+def profile() -> str:
+    """"
+        Display user logged
+        Return JSON response
+    """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-
     if user:
         return jsonify({"email": user.email}), 200
-    abort(403)
+    else:
+        abort(403)
 
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" auth module """
+"""
+Module auth
+"""
 import bcrypt
 import uuid
 from db import DB
@@ -7,38 +9,51 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """ hash password """
-    hash = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
-    return hash
+def _hash_password(password: str) -> str:
+    """
+        Hash a password
+        Return the encrypted  password
+    """
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
 
 
 def _generate_uuid() -> str:
-    """ generate uuid """
+    """
+        Generate a new UUID
+        Return the UUID generated
+    """
     return str(uuid.uuid4())
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
+    """
+        Auth class
+        create model to manage the Authentication
     """
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """ reguster user """
+        """
+            Register a user with email and pass
+            Return the User registered
+        """
         try:
             user = self._db.find_user_by(email=email)
             if user:
                 raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             pass
-
-        new_user = self._db.add_user(email, _hash_password(password))
-        return new_user
+        password = _hash_password(password)
+        user = self._db.add_user(email=email, hashed_password=password)
+        return user
 
     def valid_login(self, email: str, password: str) -> bool:
-        """ valid login """
+        """
+            Valid login with email and password
+            Return true or false
+        """
         try:
             user = self._db.find_user_by(email=email)
             password = bytes(password, 'utf-8')
@@ -46,28 +61,36 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email) -> str:
-        """ create session """
+    def create_session(self, email: str) -> str:
+        """
+            Create a session id
+            Return the session id created
+        """
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            user.session_id = session_id
-            return session_id
+            self._db.update_user(user.id, session_id=str(uuid.uuid4()))
+            return user.session_id
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id) -> User:
-        """ get user from session """
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+            Find user by session id
+            Return session id
+        """
         if session_id:
             try:
                 user = self._db.find_user_by(session_id=session_id)
                 return user
             except NoResultFound:
                 pass
-            return None
+        return None
 
     def destroy_session(self, user_id: int) -> None:
-        """ destroy session """
+        """
+            Destroy a session active
+            Return None
+        """
         if user_id:
             self._db.update_user(user_id, session_id=None)
 
