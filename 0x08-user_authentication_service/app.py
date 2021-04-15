e7741f9c66f89883d8da5031be5da4be39634e50
Camilo Moncada <monk-da@hotmail.com>
2021-04-14 19:05:32 -0500

    response question 17

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -62,5 +62,16 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+
+    try:
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token(email=email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
