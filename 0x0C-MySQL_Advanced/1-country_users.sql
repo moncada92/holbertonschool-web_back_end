b98a3fee7d0e3687cc257c25b8f73d3c48b0a42a
Camilo Moncada <monk-da@hotmail.com>
2021-04-28 17:05:49 -0500

    response question 1

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
--- create table user from holberton db
--- id email name
+-- Table Users
+-- id email name country
 
 CREATE TABLE IF NOT EXISTS users(
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') DEFAULT 'US',
   PRIMARY KEY (id)
 );
