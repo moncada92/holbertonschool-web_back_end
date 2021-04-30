4c4d71dec413714eea5393890be548838d6af5ed
Camilo Moncada <monk-da@hotmail.com>
2021-04-29 22:15:17 -0500

    bugfix question 3

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- select columns name and lifespan
 
 SELECT band_name,
-ifnull(split, YEAR(CURDATE())) - formed AS lifespan
+        ifnull(split, YEAR(CURDATE())) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC
+ORDER BY lifespan DESC;
